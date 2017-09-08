f653fdf2c193214de9ab053325eeb4c163943558
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-08 16:53:33 +0200

    save working not finished and corrected

--- dist/test.js
+++ dist/test.js
@@ -1,4 +1,14 @@
 "use strict";
 //Test unitaire.
 Object.defineProperty(exports, "__esModule", { value: true });
+var list = new UserList();
+list.add(new User("toto", "toto"));
+list.add(new User("titi", "titi"));
+if (list.user.length != 2) {
+    console.log("");
+    process.exit(1);
+}
+list.del("toto");
+console.log(list);
+var u = list.auth("titi", "titi");
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,4 +1,15 @@
 //Test unitaire.
 
-import {user} from './user;'
-import {userList} from './userList;'
+import {user} from './user;';
+import {userList} from './userList;';
+
+let list = new UserList();
+list.add(new User("toto", "toto"));
+list.add(new User("titi", "titi"));
+if(list.user.length  != 2) {
+    console.log("");
+    process.exit(1);
+}
+list.del("toto");
+console.log(list);
+let u = list.auth("titi", "titi");
